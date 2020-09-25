889f17a766d4c9ac914e6fbb64e3eb19aa84161a
geraldo vilela <geraldovilela19@gmail.com>
2020-09-25 15:11:38 -0400

    dev: useNullAsDefault: true adjust for sqlite3 default

--- knexfile.ts
+++ knexfile.ts
@@ -8,5 +8,4 @@ module.exports = {
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
-  useNullAsDefault: true,
 };
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -6,6 +6,7 @@ const connection = knex({
   connection: {
     filename: path.resolve(__dirname, 'db_feedback.sqlite'),
   },
+  useNullAsDefault: true,
 });
 
 export default connection;
